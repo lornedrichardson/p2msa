4e62677a484992f9a472289adf3afdd94a99d7e7
Alex <halos959@gmail.com>
2023-08-29 21:20:49 -0700

    Some Code & File Clean up

--- server/server.js
+++ server/server.js
@@ -11,6 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//CONTROLLER
+const gameDataControllers = require('./controllers/gameData_controller')
+app.use('api/game',gameDataControllers)
+const userDataController = require('./controllers/userData_controller')
+app.use('api/user',userDataController)
+
 //LISTEN
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
