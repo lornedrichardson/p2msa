b347393ccfc900f67cb7845a32dda5cfc5bb0c06
Alex <halos959@gmail.com>
2023-08-30 19:59:06 -0700

    Code Clean up & Home Page Created

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+// export default function Page() {
+//     return <h1>Hello, Next.js!</h1>
+//   }
+
+  export default function Page(){
+    return(
+        <div>
+            <img src="http://placekitten.com/g/50/50" alt="" />
+            <button>Login</button>
+            <div>
+                <p style={{display: 'inline-block'}}>Don't have a account? Sign up today!</p>
+                <button>Sign up</button>
+            </div>
+        </div>
+    )
+}
