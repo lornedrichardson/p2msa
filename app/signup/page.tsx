5475469c24e467a6982101adc5c652aa45f6a0eb
leecodexxe~ <lee4works@gmail.com>
2023-10-02 00:23:26 -0700

    bug fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
             })
             const data = await result.json()
             if (data.isCreate) {
-                push(`/login`)
+                push(`/`)
             } else {
                 setIsCreate(data.isCreate)
             }
